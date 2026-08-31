b60d1746d2e4f90d990e97a8906bfcf74b67014e
Rahul meena <meemohit538@gmail.com>
2026-08-31 15:11:37 +0530

    Initialize Firebase and Firestore in firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,40 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+getFirestore,
+collection,
+getDocs,
+addDoc,
+doc,
+updateDoc,
+deleteDoc,
+setDoc,
+getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyCZjafz8Ze5p6xSoir4j5IdKc_MJE_Eyns",
+authDomain: "trendy-factory.firebaseapp.com",
+projectId: "trendy-factory",
+storageBucket: "trendy-factory.firebasestorage.app",
+messagingSenderId: "811817163762",
+appId: "1:811817163762:web:c9aecc5db2cb332a13696b",
+measurementId: "G-8YDMBE9T7C"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {
+db,
+collection,
+getDocs,
+addDoc,
+doc,
+updateDoc,
+deleteDoc,
+setDoc,
+getDoc
+};
 
