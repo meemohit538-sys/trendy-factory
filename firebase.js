0c7ee1861968695515fab0b8d5cdff617adb8734
Rahul meena <meemohit538@gmail.com>
2026-09-01 00:52:30 +0530

    Refactor Firebase setup and imports
    Refactor Firebase imports and initialization for better readability.

--- firebase.js
+++ firebase.js
@@ -2,47 +2,65 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 
 import {
 getFirestore
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 import {
 getAuth
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 import {
 getStorage
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
+
 apiKey: "AIzaSyCZjafz8Ze5p6xSoir4j5IdKc_MJE_Eyns",
+
 authDomain: "trendy-factory.firebaseapp.com",
+
 projectId: "trendy-factory",
+
 storageBucket: "trendy-factory.firebasestorage.app",
+
 messagingSenderId: "811817163762",
+
 appId: "1:811817163762:web:c9aecc5db2cb332a13696b",
+
 measurementId: "G-8YDMBE9T7C"
+
 };
 
 /* ================= INITIALIZE FIREBASE ================= */
 
-const app = initializeApp(firebaseConfig);
+const app =
+initializeApp(firebaseConfig);
 
 /* ================= FIRESTORE ================= */
 
-const db = getFirestore(app);
+const db =
+getFirestore(app);
 
-/* ================= AUTH ================= */
+/* ================= AUTHENTICATION ================= */
 
-const auth = getAuth(app);
+const auth =
+getAuth(app);
 
 /* ================= STORAGE ================= */
 
-const storage = getStorage(app);
+const storage =
+getStorage(app);
 
 /* ================= EXPORT ================= */
 
 export {
-app,
+
 db,
+
 auth,
+
 storage
+
 };
